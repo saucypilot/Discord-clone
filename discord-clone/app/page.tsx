ed737e522cd98a962eacf91279a934d49ecf981f
Irian Durian <durianirian@gmail.com>
2023-12-21 01:23:18 -0600

    Set the environment up for my project

---
+++ discord-clone/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">Discord Clone</h1>
+    </div>
+  )
+}
