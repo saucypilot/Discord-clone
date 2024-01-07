5d63af84779e1dbc88768c20471f48d9f12d2d17
Irian Durian <durianirian@gmail.com>
2024-01-07 14:25:48 -0600

    Update initial-modal.tsx

--- discord-clone/components/modals/initial-modal.tsx
+++ discord-clone/components/modals/initial-modal.tsx
@@ -1,16 +1,41 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
-import { DialogDescription } from "@radix-ui/react-dialog";
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required",
+  }),
+});
 
 export const InitialModal = () => {
-  return
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+
+  return;
   <Dialog open>
     <DialogContent className="bg-white text-black p-0 overflow-hidden">
       <DialogHeader className="pt-8 px-6">
@@ -18,8 +43,8 @@ export const InitialModal = () => {
           Customize your server
         </DialogTitle>
         <DialogDescription className="text-center text-zinc-500">
-          Give your server a personality with a name and an image.
-           You can always change it later.
+          Give your server a personality with a name and an image. You can
+          always change it later.
         </DialogDescription>
       </DialogHeader>
     </DialogContent>
